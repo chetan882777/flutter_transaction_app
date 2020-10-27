8992039aa660805a8016372d2148938539b773dc
chetan pawar <chetan882777@gmail.com>
2020-10-28 01:55:53 +0530

    add transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import './transactions.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  final List<Transactions> transactions = [
+    Transactions(id: 't1', title: 'New SHoes', amount: 69.99, date: DateTime.now()),
+    Transactions(id: 't2', title: 'Weekly groceries', amount: 15.49, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
